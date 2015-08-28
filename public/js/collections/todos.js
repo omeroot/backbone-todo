c0d9ae0f36e8fc26056c6a44eb8a7befff5a90c8
omeroot <demircanomer91@gmail.com>
2015-08-28 18:56:04 +0300

    collec

--- public/js/collections/todos.js
+++ public/js/collections/todos.js
@@ -3,5 +3,13 @@ var app = app || {}
 
 app.TodoList = Backbone.Collection.extend({
   model : app.Todo,
-  localStorage : new Backbone.LocalStorage('todos-backbones')
+  localStorage : new Backbone.LocalStorage('todos-backbones'),
+  completed : function(){
+    return this.filter(function( todo ){
+      return this.get('completed');
+    })
+  },
+  remaining : function(){
+    return this.without.apply(this, this.completed());
+  }
 });
